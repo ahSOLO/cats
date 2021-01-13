1d8a98f7cb071daa290122853f8395345cad6702
ahSOLO <junmenghan@gmail.com>
2021-01-12 16:50:01 -0800

    stretch assignment

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -7,11 +7,11 @@ const breedDetailsFromFile = function(breed, callback) {
     console.log("In readFile's Callback: it has the data.");
     // CHANGE: Pass data into callback instead of reading it directly
     if (!error) callback(data);
+    else if (error) callback(undefined);
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
-// we try to get the return value
-const bombay = breedDetailsFromFile('Bombay', (data) => {console.log(data)});
-console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+
+module.exports = breedDetailsFromFile;
